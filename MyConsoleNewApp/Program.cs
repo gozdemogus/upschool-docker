1d399ef5f1526370f5f9d4311e0cb3caef6130e0
gozdemogus <gozdemog@gmail.com>
2023-01-12 00:25:06 +0300

    first commit

---
+++ MyConsoleNewApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyConsoleNewApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 1000)
+            {
+                Console.WriteLine(i + "Merhaba ankara");
+                i++;
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
+
